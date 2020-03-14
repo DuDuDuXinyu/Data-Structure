1704b29a974468974e00dae731eea565991851b8
杜欣宇 <2331102566@qq.com>
2020-03-14 17:59:00 +0800

    双向链表的基本操作

--- DList.c
+++ DList.c
@@ -94,18 +94,43 @@ void DListPopBack(DListNode* head)
 	free(pos);
 }
 
+//头插
 
 void DListPushFront(DListNode* head, int val)
 {
-
+	if (NULL == head)
+	{
+		return;
+	}
+	DListInsert(head->next, val);
 }
-void DListPopFront(DListNode* head, int val)
+
+//头删
+
+void DListPopFront(DListNode* head)
 {
-
+	if (NULL == head)
+	{
+		return;
+	}
+	DListErase(head->next);;
 }
+
+//查找
+
 DListNode* DListFind(DListNode* head, int val)
 {
 
+	DListNode* cur = head->next;
+	while (cur)
+	{
+		if (cur->val == val)
+		{
+			return cur;
+		}
+		cur = cur->next;
+	}
+	return NULL;
 }
 
 //任意位置的插入
@@ -115,23 +140,56 @@ void DListInsert(DListNode* pos, int val)
 {
 	DListNode* newNode = BuyDLiatNode(val);
 
+	//将新节点连接在链表上
+
 	newNode->prev = pos->prev;
 	newNode->next = pos;
+
+	//断开链表
+
 	newNode->prev->next = newNode;
 	pos->prev = newNode;
-
-
 }
 
 //任意节点的删除
 
 void DListErase(DListNode* pos)
 {
+	//判断节点是否存在
+
+	if (NULL == pos)
+	{
+		return;
+	}
 	pos->prev->next = pos->next;
 	pos->next->prev = pos->prev;
 	free(pos);
 }
+
+//链表的摧毁
+
 void DListDestroy(DListNode** head)
 {
+	//保证链表存在
+
+	if (NULL == head)
+	{
+		return;
+	}
+	DListNode* cur = (*head)->next;
+
+	//删除链表中的有效节点----头删
+
+	while (cur != *head)
+	{
+		(*head)->next = cur->next;
+		free(cur);
+		cur = (*head)->next;
+	}
+
+	//删除头节点
+
+	free(*head);
+	*head = NULL;
 
 }
