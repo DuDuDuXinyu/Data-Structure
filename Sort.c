5dafcd20d0be921222a519ff8248f00110fe0898
杜欣宇 <2331102566@qq.com>
2020-04-04 13:11:37 +0800

    排序算法

--- Sort.c
+++ Sort.c
@@ -74,6 +74,8 @@ void InSertSort(int* arr, int size)
 	}
 }
 
+//希尔排序
+
 void ShellSort(int* arr, int size)
 {
 	int gap = 3;
@@ -130,15 +132,19 @@ void fast_sort(int* arr, int left, int right)
 	fast_sort(arr, i + 1, right);
 }
 
-//选择排序
+//选择排序--不稳定
 
 void SelectSort(int* arr, int size)
 {
 	for (int i = 0; i < size - 1; i++)
 	{
+		//i来控制循环次数
+
 		int maxpos = 0;
 		for (int j = 1; j < size - 1; j++)
 		{
+			//控制遍历的末位置
+
 			if (arr[j] > arr[maxpos])
 				maxpos = j;
 		}
@@ -147,6 +153,8 @@ void SelectSort(int* arr, int size)
 			Swap(&arr[maxpos], &arr[size - 1 - i]);
 	}
 }
+
+//选择排序的优化
 
 void SelectSorOP(int* arr, int size)
 {
@@ -166,13 +174,143 @@ void SelectSorOP(int* arr, int size)
 			index++;
 		}
 
-		if(maxpos!=end)
+		//注意：最右侧位置有可能存储的是最小值
+
+		if (maxpos != end)
 			Swap(&arr[maxpos], &arr[end]);
 
-		if (minpos != end)
+		//如果最右侧存储的刚好是最小值，经过上述交换之后，最小值的位置已经改变
+		//必须更新minpos的值
+
+		if (minpos == end)
+			minpos = maxpos;
+		if (minpos != begin)
 			Swap(&arr[minpos], &arr[begin]);
 
 		begin++;
 		end--;
+	}
+}
+
+//堆排序
+
+void Swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	* b = temp;
+}
+
+void HeapADjust(int* array, int size, int parent)
+{
+	//优先标记左孩子->因为堆实际是完全二叉树
+	//完全二叉树中可能只有左孩子没有右孩子
+
+	int child = parent * 2 + 1;
+
+	while (child < size)
+	{
+		//找到最大的孩子
+
+		if (child + 1 && array[child + 1] > array[child])
+			child += 1;
+
+		//检测双亲是否比较大的孩子还大
+
+		if (array[child] > array[parent])
+		{
+			Swap(&array[parent], &array[child]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+			return;	
+	}
+}
+
+void HeapSort(int* array, int size)
+{
+	//建堆
+	//找到倒数第一个非叶子节点
+
+	for (int root = (size - 2) >> 1; root >= 0; root--)
+	{
+		//向下调整
+
+		HeapADjust(array, size, root);
+	}
+
+	//利用堆的思想来进行排序
+
+	int end = size - 1;
+	while (end)
+	{
+		Swap(&array[0], &array[end]);
+		HeapADjust(array, end, 0);
+		end--;
+	}
+}
+
+//冒泡排序
+
+void BubbleSort(int* array, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		int isChange = 0;
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				Swap(&array[j], &array[j + 1]);
+				isChange = 1;
+			}
+		}
+
+		//没有交换说明已经有序，所以无需继续遍历
+
+		if (!isChange)
+			return;
+	}
+}
+
+//快排
+
+int Partion(int* array, int left, int right)
+{
+	int begin = left;
+	int end = right - 1;
+	int key = array[end];
+	
+	while (begin < end)
+	{
+		//让begin从前往后找，比key大的元素
+
+		while (begin < end && array[begin] <= key)
+			begin++;
+
+		//让end从后往前找，比key小的元素
+
+		while (begin < end && array[end] >= key)
+			end--;
+
+		if (begin < end)
+		{
+			Swap(&array[begin], &array[end]);
+		}
+	}
+
+	if (begin != right - 1)
+		Swap(&array[begin], &array[right - 1]);
+	return begin;
+}
+
+void QuickSort(int* array, int left, int right)
+{
+	if (right - left > 1)
+	{
+		int div = Partion(array, left, right);
+		QuickSort(array, left, div);
+		QuickSort(array, div + 1, right);
 	}
 }
